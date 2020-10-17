92d02274ec6db5a9eac70b6283f072fc99fc6256
Tsotne Gvadzabia <tg289@kent.ac.uk>
2020-10-17 21:24:29 +0100

    moving box

--- Test/extension.js
+++ Test/extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +19,60 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('Test.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('Test.startAnimation', function () {
 		// The code you place here will be executed every time your command is executed
+		let input = "Input Here!";
+		const htmlContent = `
+		<svg viewBox="0 0 600 200" xmlns="http://www.w3.org/2000/svg">
+	<foreignObject width="100%" height="100%">
+		<div xmlns="http://www.w3.org/1999/xhtml">
+		<style>
+		section {
+		  width: 50px;
+		  height: 50px;
+		  background-color: red;
+		  position: relative;
+		  animation: myfirst 5s linear 1s infinite alternate;
+		}
+		
+		@keyframes myfirst {
+		  0%   {background-color:red; left:0%; top:0px;}
+		  100%  {background-color:yellow; left:90%; top:0px;}
+		}
+		</style>
+		
+	<section>`+input+`</section>
+	</div>
+ </foreignObject>
+</svg>
+		`;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test!');
+		const display = `# Welcome, this is the first test. 
+![](liveData/img.svg)`;
+		const folderPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+	
+		//Creates Folder and HTML File
+		fs.mkdir(path.join(folderPath,"liveData"),{ recursive: true }, (err) => {
+			if(err){
+				console.error(err);
+				return vscode.window.showErrorMessage("Failed to create animation file");
+			}
+			fs.writeFile(path.join(folderPath, "liveData/img.svg"), htmlContent , err => {
+				if(err){
+					console.error(err);
+					return vscode.window.showErrorMessage("Failed to create animation file");
+				}
+				vscode.window.showInformationMessage("Created Animation File");
+			});
+		});
+		//Creates MD File
+		fs.writeFile(path.join(folderPath, "Display.md"), display, err =>{
+			if(err){
+				console.error(err);
+				return vscode.window.showErrorMessage("Failed to create README file");
+			}
+			vscode.window.showInformationMessage("Created README File");
+		})
 	});
 
 	context.subscriptions.push(disposable);
